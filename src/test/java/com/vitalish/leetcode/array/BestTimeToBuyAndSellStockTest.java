55321b92b1ad516bb718ec396fa994ed8c8154ce
vitalish <vitalish@mac.local>
2024-04-15 06:53:50 +0300

    121. Best Time to Buy and Sell Stock

---
+++ src/test/java/com/vitalish/leetcode/array/BestTimeToBuyAndSellStockTest.java
@@ -0,0 +1,26 @@
+package com.vitalish.leetcode.array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class BestTimeToBuyAndSellStockTest {
+
+    private BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
+
+    @ParameterizedTest
+    @MethodSource("argumentSource")
+    void testBestTimeToBuyAndSell(int[] prises, int expected) {
+        Assertions.assertEquals(expected, bestTimeToBuyAndSellStock.maxProfit(prises));
+    }
+
+    public static Stream<Arguments> argumentSource() {
+        return Stream.of(
+                Arguments.of(new int[]{7, 1, 5, 3, 6, 4}, 5),
+                Arguments.of(new int[]{7, 6, 4, 3, 1}, 0)
+        );
+    }
+}
