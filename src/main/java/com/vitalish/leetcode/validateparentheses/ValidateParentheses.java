e2ae8b1b2501d8ab4905a734be0d35bd22404de2
vitalish <vitalish@mac.local>
2024-10-08 08:44:13 +0300

    Validate Parentheses

---
+++ src/main/java/com/vitalish/leetcode/validateparentheses/ValidateParentheses.java
@@ -0,0 +1,36 @@
+package com.vitalish.leetcode.validateparentheses;
+
+import java.util.ArrayDeque;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @see https://neetcode.io/problems/validate-parentheses
+ */
+public class ValidateParentheses {
+
+    private static Map<Character, Character> parentheses = Map.of(
+            ')', '(',
+            '}', '{',
+            ']', '['
+    );
+
+    public boolean isValid(String s) {
+        var stack = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            var symbol = s.charAt(i);
+            if (parentheses.containsKey(symbol)) {
+                if (!stack.isEmpty() && Objects.equals(stack.getFirst(), parentheses.get(symbol))) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(symbol);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
