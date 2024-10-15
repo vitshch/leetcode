f422d434deeab08d26ebb51705d80249b80eeba5
vitalish <vitalish@mac.local>
2024-10-15 11:29:22 +0300

    Day 5 :: 1544. Make The String Great

---
+++ src/test/java/com/vitalish/leetcode/stack/MakeTheStringGreatTest.java
@@ -0,0 +1,30 @@
+package com.vitalish.leetcode.stack;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MakeTheStringGreatTest {
+
+    private static final MakeTheStringGreat makeTheStringGreat = new MakeTheStringGreat();
+
+    @ParameterizedTest
+    @MethodSource("arguments")
+    void makesTheStringGreat(String input, String expected) {
+        assertEquals(expected, makeTheStringGreat.makeGood(input));
+    }
+
+    private static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.of("aAbBcc", "cc"),
+                Arguments.of("leEeetcode", "leetcode"),
+                Arguments.of("abBAcC", ""),
+                Arguments.of("s", "s")
+        );
+    }
+
+}
