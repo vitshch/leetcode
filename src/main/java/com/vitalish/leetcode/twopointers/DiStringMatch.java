c2e88dfa3b9a9d19d311a8a58cd16caf9a1cf7c4
vitalish <vitalish@mac.local>
2025-02-10 09:35:42 +0200

    942. DI String Match Algorithm Challenge[https://algorithm-challenge.com/]

---
+++ src/main/java/com/vitalish/leetcode/twopointers/DiStringMatch.java
@@ -0,0 +1,27 @@
+package com.vitalish.leetcode.twopointers;
+
+/**
+ * 942. DI String Match
+ *
+ * @see https://leetcode.com/problems/di-string-match/description/
+ */
+public class DiStringMatch {
+
+    public int[] diStringMatch(String s) {
+        int size = s.length();
+        int low = 0, hi = size;
+
+        int[] result = new int[size + 1];
+        for (int i = 0; i < size; ++i) {
+            if (s.charAt(i) == 'I') {
+                result[i] = low++;
+            } else {
+                result[i] = hi--;
+            }
+        }
+
+        result[size] = low;
+        return result;
+    }
+
+}
