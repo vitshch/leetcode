50a2eb7c185cf214b2bcbdc235b7c249fad598b1
vitalish <vitalish@mac.local>
2025-02-15 10:32:58 +0200

    2389. Longest Subsequence With Limited Sum Algorithm Challenge[https://algorithm-challenge.com/]

---
+++ src/test/java/com/vitalish/leetcode/binarysearch/LongestSubsequenceWithLimitedSumTest.java
@@ -0,0 +1,27 @@
+package com.vitalish.leetcode.binarysearch;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class LongestSubsequenceWithLimitedSumTest {
+
+    LongestSubsequenceWithLimitedSum solution = new LongestSubsequenceWithLimitedSum();
+
+    @ParameterizedTest
+    @MethodSource("arguments")
+    void testSolution(int[] nums, int[] queries, int[] expected) {
+        assertArrayEquals(expected, solution.answerQueries(nums, queries));
+    }
+
+    private static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.of(new int[]{4, 5, 2, 1}, new int[]{3, 10, 21}, new int[]{2, 3, 4}),
+                Arguments.of(new int[]{2, 3, 4, 5}, new int[]{1}, new int[]{0})
+        );
+    }
+}
